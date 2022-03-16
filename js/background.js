72e60fe60d3a0df733eeaf2f4a44f16534c0b8ab
SeungHwan Hong <mitzbarbie@naver.com>
2022-03-16 12:24:50 +0900

    CSS : clock position & background

--- js/background.js
+++ js/background.js
@@ -18,6 +18,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-console.log(bgImage);
+// console.log(bgImage);
 
 document.body.appendChild(bgImage);
